9d25dd9fecf664cbc252a4fa7244578123c168a1
Darren Xu <darrenxu1994@yahoo.com.au>
2019-11-02 14:33:38 +1100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <timeline />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Timeline from "./components/Timeline";
+require("swiper/js/swiper.min.js");
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  components: { Timeline }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
